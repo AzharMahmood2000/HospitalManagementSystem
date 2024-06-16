cf29aaaf22f145090a3b739fbb65c3f3c55dcc5a
AzharMahmood2000 <163389254+AzharMahmood2000@users.noreply.github.com>
2024-06-16 21:36:40 +0530

    Initial commit

---
+++ Controller/preparedStatement.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+/**
+ *
+ * @author User
+ */
+class preparedStatement {
+
+    void setSring(int i, String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setString(int i, String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
